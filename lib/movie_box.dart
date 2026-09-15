5a8d17cb6241ea34b22f352e96501f1428284450
AbdelwahabEmad <abd916587@gmail.com>
2026-09-15 10:28:23 +0300

    build intial structure of movie app

---
+++ lib/movie_box.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MovieBox extends StatelessWidget {
+  const MovieBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
