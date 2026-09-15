4b83e39de13beb0f7417cb298b4f7e2b87e7ce4c
AbdelwahabEmad <abd916587@gmail.com>
2026-09-15 11:43:04 +0300

    feat: create UserModel

---
+++ lib/Features/auth/data/models/user_model.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  final String uid;
+  final String name;
+  final String email;
+
+  UserModel({required this.uid, required this.name, required this.email});
+
+  factory UserModel.fromFirebaseUser({
+    required String uid,
+    required String? displayName,
+    required String? email,
+  }) {
+    return UserModel(uid: uid, name: displayName ?? '', email: email ?? '');
+  }
+}
